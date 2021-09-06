eadc28a446f3e7f6901d41274b45b74868f9602c
shivank goyal <71492552+shivankgoyal790@users.noreply.github.com>
2021-09-06 10:34:37 +0530

    Update forloop.cpp

--- forloop.cpp
+++ forloop.cpp
@@ -1,4 +1,30 @@
-its for loop
+#include <iostream>
+using namespace std;
 
-files changed
-files changes second time
+// Function to demonstrate printing pattern
+void pypart(int n)
+{
+	// Outer loop to handle number of rows
+	// n in this case
+	for (int i = 0; i < n; i++) {
+
+		// Inner loop to handle number of columns
+		// values changing acc. to outer loop
+		for (int j = 0; j <= i; j++) {
+
+			// Printing stars
+			cout << "* ";
+		}
+
+		// Ending line after each row
+		cout << endl;
+	}
+}
+
+// Driver Function
+int main()
+{
+	int n = 5;
+	pypart(n);
+	return 0;
+}
